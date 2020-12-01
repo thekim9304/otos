fb612e6e78e6806033bfcd9c7a5d3ed2fba09212
TaeHyung <th_k9304@naver.com>
2020-12-01 16:10:49 +0900

    final

--- recording.py
+++ recording.py
@@ -14,7 +14,7 @@ class MyApp(QMainWindow, main_ui):
         self.initUI()
 
         # hyper parameter
-        self.wait_rate = 1
+        self.wait_ms = 1
         self.plot_limit = -150
         self.thresh = [180, 255]  # [min, max]
 
@@ -38,6 +38,7 @@ class MyApp(QMainWindow, main_ui):
 
         # 녹화 변수
         self.recording = False
+        self.stop_recording = False
         self.saved_dir = ''
         self.saved_name = ''
         self.total_frame = 0
@@ -49,62 +50,16 @@ class MyApp(QMainWindow, main_ui):
         self.pushButton_cam.clicked.connect(self.camSetting_button)
         self.pushButton_startRecording.clicked.connect(self.startRecording_button)
         self.pushButton_selectDirectory.clicked.connect(self.selectDirectory_button)
+        self.pushButton_recordingStop.clicked.connect(self.recordingStop_button)
         self.horizontalSlider_max.valueChanged.connect(self.maxThresh)
         self.horizontalSlider_min.valueChanged.connect(self.minThresh)
         self.comboBox_cam.currentIndexChanged.connect(self.camSetting_combo)
 
-    def selectDirectory_button(self):
-        self.saved_dir = QFileDialog.getExistingDirectory(self, 'Select save directory', 'C:/')
-        self.label_saveDirectory.setText(self.saved_dir)
-
-    def startRecording_button(self):
-        self.recording_time = self.plainTextEdit_recording.toPlainText()
-        self.recording_time = list(map(int, self.recording_time.split('.')))
-
-        self.saved_name = self.plainTextEdit_name.toPlainText()
-        self.saved_dir = self.label_saveDirectory.text()
-        file_check = False
-        if self.saved_dir:
-            files = os.listdir(self.saved_dir)
-            if f'{self.saved_name}.avi' not in files:
-                file_check = True
-
-        if self.recording_time and file_check:
-            self.total_frame = self.recording_time[0] * 60 * 30
-            self.total_frame += self.recording_time[1] * 30
-            self.record_frame_cnt = 0
-            print(f'{self.saved_dir}/{self.saved_name}.avi')
-            self.frame_out = cv2.VideoWriter(f'{self.saved_dir}/{self.saved_name}.avi',
-                                             fourcc,
-                                             30.0,
-                                             (640, 480))
-            self.recording = True
-        else:
-            pass
-
-    def camSetting_button(self):
-        self.cap = cv2.VideoCapture(self.cam_num)
-
-        if self.cap.isOpened():
-            self.fig = plt.figure()
-            self.plot_xs = []
-            self.plot_ys = []
-            self.max_y = 0
-
-            self.startMeasurement()
-        else:
-            self.cap = None
-
-    def camSetting_combo(self):
-        self.cam_num = int(self.comboBox_cam.currentText())
-
     def startMeasurement(self):
         self.change_video = False
         if self.cap:
-            num_of_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
             frame_cnt = 0
             while True:
-                curTime = time.time()
                 if self.press_esc or self.change_video or self.clicked:
                     break
 
@@ -112,6 +67,10 @@ class MyApp(QMainWindow, main_ui):
                 if self.display_img:
                     # idx_of_frame = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
                     if self.roi_coord:
+                        if self.roi_coord[0] > self.roi_coord[2]:
+                            self.roi_coord[0], self.roi_coord[2] = self.roi_coord[2], self.roi_coord[0]
+                        if self.roi_coord[1] > self.roi_coord[3]:
+                            self.roi_coord[1], self.roi_coord[3] = self.roi_coord[3], self.roi_coord[1]
                         x1, y1, x2, y2 = self.roi_coord
                         height, width, _ = self.ori_img.shape
                         x1, y1, x2, y2 = int(x1 * width), int(y1 * height), int(x2 * width), int(y2 * height)
@@ -136,23 +95,25 @@ class MyApp(QMainWindow, main_ui):
                     # graph = cv2.rotate(graph, cv2.ROTATE_90_CLOCKWISE)
 
                     if self.recording:
-                        # self.ori_img 저장하면됨
-                        self.frame_out.write(self.ori_img)
-                        show_str = f'{frames_to_timecode(self.record_frame_cnt)}/{frames_to_timecode(self.total_frame)}'
-                        self.label_recordTime.setText(show_str)
-                        self.progressBar.setValue(int((self.record_frame_cnt / self.total_frame) * 100))
-                        if self.record_frame_cnt == self.total_frame:
+                        if self.stop_recording:
                             self.recording = False
                             self.frame_out.release()
+                        else:
+                            # self.ori_img 저장하면됨
+                            self.frame_out.write(self.ori_img)
+                            show_str = f'{frames_to_timecode(self.record_frame_cnt)}/{frames_to_timecode(self.total_frame)}'
+                            self.label_recordTime.setText(show_str)
+                            self.progressBar.setValue(int((self.record_frame_cnt / self.total_frame) * 100))
+                            if self.record_frame_cnt == self.total_frame:
+                                self.recording = False
+                                self.frame_out.release()
 
                     self._showImage(self.ori_img, self.display_label)
                     self._showImage(binary_eye, self.display_binary)
-                    # self._showImage(graph, self.display_graph)
-                    # self.progressBar.setValue(int((idx_of_frame / num_of_frames) * 100))
 
                     frame_cnt += 1
                     self.record_frame_cnt += 1
-                    cv2.waitKey(self.wait_rate)
+                    cv2.waitKey(self.wait_ms)
                 else:
                     break
 
@@ -189,10 +150,10 @@ class MyApp(QMainWindow, main_ui):
             if 0 <= rel_x <= 1 and 0 <= rel_y < 1:
                 self.clicked = True
                 self.roi_coord = [rel_x, rel_y, rel_x, rel_y]
-            else:
-                self.clicked = False
-                self.roi_coord = []
-                self.pupil_info = []
+            # else:
+            #     self.clicked = False
+            #     self.roi_coord = []
+            #     self.pupil_info = []
 
             self._showImage(self.ori_img, self.display_label)
 
@@ -219,16 +180,69 @@ class MyApp(QMainWindow, main_ui):
     def mouseReleaseEvent(self, event):
         if self.clicked:
             self.clicked = False
-            self.fig = plt.figure()
+            # self.fig = plt.figure()
             self.plot_xs = []
             self.plot_ys = []
             self.max_y = 0
+            if self.roi_coord[0] == self.roi_coord[2] and self.roi_coord[1] == self.roi_coord[3]:
+                self.roi_coord = []
             self.startMeasurement()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             self.press_esc = True
             self.close()
+
+    def recordingStop_button(self):
+        self.stop_recording = True
+
+    def selectDirectory_button(self):
+        self.saved_dir = QFileDialog.getExistingDirectory(self, 'Select save directory', 'C:/')
+        self.label_saveDirectory.setText(self.saved_dir)
+
+    def startRecording_button(self):
+        self.stop_recording = False
+        self.recording_time = self.plainTextEdit_recording.toPlainText()
+
+        self.saved_name = self.plainTextEdit_name.toPlainText()
+        self.saved_dir = self.label_saveDirectory.text()
+        file_check = False
+        if self.saved_dir and self.saved_name:
+            files = os.listdir(self.saved_dir)
+            if f'{self.saved_name}.avi' not in files:
+                file_check = True
+
+        if self.recording_time:
+            self.recording_time = list(map(int, self.recording_time.split('.')))
+            if len(self.recording_time) == 2:
+                if (self.recording_time[0]+self.recording_time[1]) and file_check:
+                    self.total_frame = self.recording_time[0] * 60 * 30
+                    self.total_frame += self.recording_time[1] * 30
+                    self.record_frame_cnt = 0
+                    print(f'{self.saved_dir}/{self.saved_name}.avi')
+                    self.frame_out = cv2.VideoWriter(f'{self.saved_dir}/{self.saved_name}.avi',
+                                                     fourcc,
+                                                     30.0,
+                                                     (640, 480))
+                    self.recording = True
+        else:
+            pass
+
+    def camSetting_button(self):
+        self.cap = cv2.VideoCapture(self.cam_num)
+
+        if self.cap.isOpened():
+            # self.fig = plt.figure()
+            self.plot_xs = []
+            self.plot_ys = []
+            self.max_y = 0
+
+            self.startMeasurement()
+        else:
+            self.cap = None
+
+    def camSetting_combo(self):
+        self.cam_num = int(self.comboBox_cam.currentText())
 
     def maxThresh(self):
         self.thresh[1] = self.horizontalSlider_max.value()
@@ -244,25 +258,25 @@ class MyApp(QMainWindow, main_ui):
         self.horizontalSlider_min.setValue(self.thresh[0])
         self.label_minThr.setText(f'{self.thresh[0]}')
 
-    def getGraph(self, xs, ys):
-        max_y = max(ys)
-        if self.max_y < max_y:
-            self.max_y = max_y
-
-        ax = self.fig.add_subplot(1, 1, 1)
-
-        ax.clear()
-        ax.plot(xs, ys)
-        plt.ylim(-1, self.max_y + 10)
-        plt.xticks(rotation=45, ha='right')
-        plt.ylabel('Pupil size')
-
-        self.fig.canvas.draw()
-        img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-        img = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-        return img
+    # def getGraph(self, xs, ys):
+    #     max_y = max(ys)
+    #     if self.max_y < max_y:
+    #         self.max_y = max_y
+    #
+    #     ax = self.fig.add_subplot(1, 1, 1)
+    #
+    #     ax.clear()
+    #     ax.plot(xs, ys)
+    #     plt.ylim(-1, self.max_y + 10)
+    #     plt.xticks(rotation=45, ha='right')
+    #     plt.ylabel('Pupil size')
+    #
+    #     self.fig.canvas.draw()
+    #     img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    #     img = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
+    #     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    #
+    #     return img
 
     def _center(self):
         qr = self.frameGeometry()
